a44ac9797ceef9d56c24c9463fb3d3af7eaae310
aceforeverd <dace1p@gmail.com>
2018-05-04 20:43:41 +0800

    refactor to maven project

--- src/test/java/yqqs/unitconverter/LengthTests.java
+++ src/test/java/yqqs/unitconverter/LengthTests.java
@@ -1,5 +1,5 @@
 import org.junit.jupiter.api.Test;
-import measurement.*;
+import yqqs.unitconverter.*;
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
